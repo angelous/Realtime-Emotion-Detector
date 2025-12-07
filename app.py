20894f052b9b7ec6d538c75febff61d3ac46b87f
Angela Verine Hutomo <angelaverine@gmail.com>
2025-12-07 13:03:41 +0700

    update app.py to expand STUN server configuration for WebRTC

--- app.py
+++ app.py
@@ -50,7 +50,13 @@ webrtc_ctx = webrtc_streamer(
     key="yolo-drowsy",
     mode=WebRtcMode.SENDRECV,
     rtc_configuration={
-        "iceServers": [{ "urls": "stun:stun4.l.google.com:19302" }]
+        "iceServers": [
+            { "urls": "stun:stun.l.google.com:19302" },
+            { "urls": "stun:stun1.l.google.com:19302" },
+            { "urls": "stun:stun2.l.google.com:19302" },
+            { "urls": "stun:stun3.l.google.com:19302" },
+            { "urls": "stun:stun4.l.google.com:19302" }
+        ]
     },
     video_processor_factory=YOLOTransformer,
     media_stream_constraints={"video": True, "audio": False},
