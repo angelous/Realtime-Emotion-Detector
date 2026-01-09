bf4907f4ea510f1dc26502774eab37b8f9334bb2
Angela Verine Hutomo <angelaverine@gmail.com>
2026-01-09 12:19:10 +0700

    Update WebRTC configuration with additional STUN/TURN servers and add opencv-python to requirements

--- app.py
+++ app.py
@@ -87,7 +87,29 @@ webrtc_ctx = webrtc_streamer(
     mode=WebRtcMode.SENDRECV,
     rtc_configuration={
         "iceServers": [
-            {"urls": ["stun:stun.l.google.com:19302"]}
+            {
+                "urls": "stun:stun.relay.metered.ca:80",
+            },
+            {
+                "urls": "turn:global.relay.metered.ca:80",
+                "username": "9a6ad3c8facce6c9e0e03901",
+                "credential": "h5l6AzsvKLBAGlLD",
+            },
+            {
+                "urls": "turn:global.relay.metered.ca:80?transport=tcp",
+                "username": "9a6ad3c8facce6c9e0e03901",
+                "credential": "h5l6AzsvKLBAGlLD",
+            },
+            {
+                "urls": "turn:global.relay.metered.ca:443",
+                "username": "9a6ad3c8facce6c9e0e03901",
+                "credential": "h5l6AzsvKLBAGlLD",
+            },
+            {
+                "urls": "turns:global.relay.metered.ca:443?transport=tcp",
+                "username": "9a6ad3c8facce6c9e0e03901",
+                "credential": "h5l6AzsvKLBAGlLD",
+            },
         ]
     },
     video_processor_factory=YOLOTransformer,
